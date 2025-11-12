b2ee3ff63046f7133df667a783e528f364eb3015
DESKTOP-FR144OA\Anubis <adamas87@gmail.com>
2025-11-12 12:42:22 +0800

    'Fix'

--- task.sql
+++ task.sql
@@ -50,5 +50,5 @@ CREATE TABLE Users (
 --rollback DROP TABLE Users;
 
 --changeset mate.academy:6 labels:0.0.3
-CREATE INDEX idx_users_email ON Users(Email);
+CREATE INDEX Email ON Users(Email);
 --rollback DROP INDEX idx_users_email ON Users;
